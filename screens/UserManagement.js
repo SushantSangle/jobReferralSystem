aeb9cf9cc90d3fae7fcc2aa21006ec7c5c81e5e2
Hardika3 <haardyy3@gmail.com>
2020-09-09 21:26:13 +0530

    adding the pull section of user management

--- screens/UserManagement.js
+++ screens/UserManagement.js
@@ -24,8 +24,6 @@ import OptionsMenu from "react-native-options-menu";
 
 import RoleManager from '../utils/RoleManager'
 
-
-
 const Data = [
     {
 
@@ -69,8 +67,42 @@ export default class UserManagement extends Component {
         super(props);
         this.navigation = this.props.navigation;
         this.state = {
+            loading: true,
+            data: [],
+            page: 1,
+            seed: 1,
+            error: null,
+            refreshing: false,
             roleLevel: RoleManager.getLevel(),
         };
+    }
+    componentDidMount(){
+        this.setState({ loading: true });
+        var query = new Query("employeeData");
+        var eachPromise = query.each((result) => {
+            
+            this.state.data.push({
+                userId: result.get('auth'),
+                userName: result.get('firstname')+ " "+ result.get('lastname'),
+                userPost: result.get("Designation"),
+                userWorkExperience:result.get("workExperience"),
+                userGender:result.get('gender'),
+                userDateofbirth:result.get('dob'),
+                userMobile:result.get('phone'),
+                userAddress:result.get('address'),
+            });
+            this.setState(this.state);
+            console.log("data read");
+        })
+        eachPromise.then((result) => {
+            console.log("data promise fulfilled");
+        }, (errorin) => {
+            this.setState({ error: errorin, loading: false });
+            console.log("data Promise ERROR:" + error);
+        }).catch((errorin) => {
+            this.setState({ error: errorin, loading: false });
+            console.log("data Promise ERROR:" + error);
+        })
     }
 
     render() {
@@ -94,20 +126,21 @@ export default class UserManagement extends Component {
         );
     }
 
-    renderItem = ({ item }) => {
+    render () {
+        let posts = this.state.data.map((val,key)=>{
+            console.log(key);
         return(
                 <TouchableOpacity style={{ elevation: 10 }} 
                 onPress={() =>
                     this.navigation.navigate("UserDetails", {
-                        userName: item.userName,
-                        userId: item.userId,
-                        userPost: item.userPost,
-                        userWorkExperience: item.userWorkExperience,
-                        userPassword: item.userPassword,
-                        userGender: item.userGender,
-                        userDateofbirth: item.userDateofbirth,
-                        userMobile: item.userMobile,
-                        userAddress: item.userAddress,
+                        userName: val.userName,
+                        userId: val.userId,
+                        userPost: val.userPost,
+                        userWorkExperience: val.userWorkExperience,
+                        userGender: val.userGender,
+                        userDateofbirth: val.userDateofbirth,
+                        userMobile: val.userMobile,
+                        userAddress: val.userAddress,
                     })
                 } >
 
@@ -115,18 +148,18 @@ export default class UserManagement extends Component {
             
                             <Text style={styles1.jobcard_head}>{item.userName}</Text>
                             
-                            <Text style={styles1.jobcard_details}>POST: {item.userPost}</Text>
-                            <Text style={styles1.jobcard_details}>ID: {item.userId}</Text>
-                            <Text style={styles1.jobcard_details}>POST: {item.userPost}</Text>
-                            <Text style={styles1.jobcard_details}>WORK EXP: {item.userWorkExperience}</Text>
-                            <Text style={styles1.jobcard_details}>PASSWORD: {item.userPassword}</Text>
-                            <Text style={styles1.jobcard_details}>GENDER: {item.userGender}</Text>
-                            <Text style={styles1.jobcard_details}>MOBILE: {item.userMobile}</Text>
-                            <Text style={styles1.jobcard_details}>ADDRESS: {item.userAddress}</Text>
+                            <Text style={styles1.jobcard_details}>POST: {val.userPost}</Text>
+                            <Text style={styles1.jobcard_details}>ID: {val.userId}</Text>
+                            <Text style={styles1.jobcard_details}>POST: {val.userPost}</Text>
+                            <Text style={styles1.jobcard_details}>WORK EXP: {val.userWorkExperience}</Text>
+                            <Text style={styles1.jobcard_details}>GENDER: {val.userGender}</Text>
+                            <Text style={styles1.jobcard_details}>MOBILE: {val.userMobile}</Text>
+                            <Text style={styles1.jobcard_details}>ADDRESS: {val.userAddress}</Text>
 
                         </View>
                     </TouchableOpacity>
         );
+    });
     
     }
     onPress = () => {
